c41d0a9fb7122d20373aed5cb85be4100a22ed54
matanki.saito <matanki.saito@gmail.com>
2020-06-20 01:00:49 +0900

    置き換える処理を入れた

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import hashlib
 import json
 import os
+import pathlib
+import re
 import shutil
 import tempfile
 import urllib.request
@@ -13,7 +15,32 @@ from os.path import join
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+from special_escape import generate_printer, generate_encoder
+
+encoder = generate_encoder("eu4", "txt")
+printer = generate_printer("eu4", "txt")
+
 _ = join
+
+"""
+$ 階級について
+こちら参照のこと：https://gist.github.com/matanki-saito/a2afb80eeeef612a28426af31e226d3d
+"""
+replace_estate_parameter_map_definition = {
+    "GetNobilityName": "Get貴族Name",
+    "GetClergyName": "Get聖職者Name",
+    "GetBurghersName": "Get市民Name",
+
+    "GetCossacksName": "GetコサックName",
+    "GetDhimmiName": "GetズィンミーName",
+    "GetTribesName": "Get部族Name",
+
+    "GetBrahminsName": "GetバラモンName",
+    "GetJainsName": "Getジャイナ教徒Name",
+    "GetMarathasName": "GetマラーターName",
+    "GetRajputsName": "GetラージプートName",
+    "GetVaishyasName": "GetヴァイシャName"
+}
 
 
 def upload_mod_to_google_drive(upload_file_path,
@@ -81,13 +108,13 @@ def download_trans_zip_from_paratranz(project_id,
 
 
 def assembly_app_mod_zip_file(resource_image_file_path,
-                              resource_paratranz_trans_zip_file_path,
-                              out_file_path):
+                              source_dir_path,
+                              out_directory_path):
     """
     Appモッドを作成
-    :param resource_paratranz_trans_zip_file_path: Paratranzからダウンロードできるzipファイルのパス
+    :param source_dir_path:
     :param resource_image_file_path: 画像ファイルパス
-    :param out_file_path: 出力ファイルパス
+    :param out_directory_path: 出力ファイルパス
     :return:
     """
 
@@ -95,30 +122,47 @@ def assembly_app_mod_zip_file(resource_image_file_path,
         # 画像ファイル
         shutil.copy(resource_image_file_path, temp_dir_path)
 
-        # localisation
-        salvage_files_from_paratranz_trans_zip(out_dir_path=_(temp_dir_path, "localisation"),
-                                               folder_list=["localisation"],
-                                               paratranz_zip_path=resource_paratranz_trans_zip_file_path,
-                                               head_folder_name="special")
+        shutil.copytree(source_dir_path, _(temp_dir_path, 'localisation'))
 
         # zip化する
-        return shutil.make_archive(out_file_path, 'zip', root_dir=temp_dir_path)
+        return shutil.make_archive(_(out_directory_path, 'mod'), 'zip', root_dir=temp_dir_path)
 
 
-def update_source(resource_paratranz_trans_zip_file_path):
-    shutil.rmtree("source", ignore_errors=True)
-    os.makedirs("source", exist_ok=True)
+def replace_estate_parameters(replace_estate_parameter_map,
+                              source_dir_path,
+                              output_dir_path):
+    """
+    特殊なパラメータを置き換える
+    :param replace_estate_parameter_map:
+    :param source_dir_path:
+    :param output_dir_path:
+    :return:
+    """
 
-    salvage_files_from_paratranz_trans_zip(out_dir_path=_("source", "localisation"),
-                                           folder_list=["localisation"],
-                                           paratranz_zip_path=resource_paratranz_trans_zip_file_path,
-                                           head_folder_name="utf8")
+    shutil.rmtree(output_dir_path, ignore_errors=True)
+    os.makedirs(output_dir_path, exist_ok=True)
+
+    match_pattern = re.compile("{}".format(
+        r"|".join(replace_estate_parameter_map.keys())
+    ))
+
+    def repl(match_obj):
+        return replace_estate_parameter_map.get(match_obj.group(0))
+
+    for source_file_path in pathlib.Path(source_dir_path).glob('**/*.yml'):
+        with open(str(source_file_path), 'rt', encoding='utf_8_sig', errors='ignore', newline='') as f:
+            replaced_source_file_text = re.sub(match_pattern, repl, f.read())
+            write_file_path = str(_(output_dir_path, source_file_path.name))
+            with open(write_file_path, 'wt', encoding='utf_8_sig') as w:
+                w.write(replaced_source_file_text)
 
 
 def salvage_files_from_paratranz_trans_zip(out_dir_path,
                                            paratranz_zip_path,
                                            folder_list,
                                            head_folder_name):
+    shutil.rmtree(out_dir_path, ignore_errors=True)
+
     with zipfile.ZipFile(paratranz_zip_path) as paratranz_zip:
         special_files = filter(lambda name: name.startswith(head_folder_name + "/"), paratranz_zip.namelist())
 
@@ -209,24 +253,63 @@ def pack_mod(out_file_path,
         return shutil.make_archive(out_file_path, 'zip', root_dir=temp_dir_path)
 
 
+def special_escape(source_dir_path, output_dir_path):
+    shutil.rmtree(output_dir_path, ignore_errors=True)
+    os.makedirs(output_dir_path, exist_ok=True)
+
+    for source_file_path in pathlib.Path(source_dir_path).glob('**/*.yml'):
+        printer(src_array=encoder(src_array=map(ord, source_file_path.read_text(
+            encoding="utf-8"
+        ))), out_file_path=_(output_dir_path, source_file_path.name))
+
+
 def main():
     # 一時フォルダ用意
-    os.makedirs(_(".", "tmp"), exist_ok=True)
+    tmp_directory_path = _(".", "tmp")
+    os.makedirs(tmp_directory_path, exist_ok=True)
+
+    # 出力フォルダ用意
     os.makedirs(_(".", "out"), exist_ok=True)
 
     # 翻訳の最新版をダウンロードする
-    p_file_path = download_trans_zip_from_paratranz(
-        project_id=76,
-        secret=os.environ.get("PARATRANZ_SECRET"),
-        out_file_path=_(".", "tmp", "paratranz.zip"))
+    p_file_path = _(".", "tmp", "paratranz.zip")
+    if not os.path.exists(p_file_path):
+        download_trans_zip_from_paratranz(
+            project_id=76,
+            secret=os.environ.get("PARATRANZ_SECRET"),
+            out_file_path=p_file_path)
 
     print("p_file_path:{}".format(p_file_path))
 
+    # utf8ファイルを抽出する（この後git pushするのにも使う）
+    localisation_dir_path = _(tmp_directory_path, "source", "localisation")
+    salvage_files_from_paratranz_trans_zip(out_dir_path=localisation_dir_path,
+                                           folder_list=["localisation"],
+                                           paratranz_zip_path=p_file_path,
+                                           head_folder_name="utf8")
+
+    print("Finish extracting localisation dir")
+
+    # 特殊なキーワードを置き換える
+    replaced_localization_dir_path = _(tmp_directory_path, "replaced_localization")
+    replace_estate_parameters(replace_estate_parameter_map=replace_estate_parameter_map_definition,
+                              source_dir_path=localisation_dir_path,
+                              output_dir_path=replaced_localization_dir_path)
+
+    print("Finish replacing special keywords")
+
+    # 特殊エンコードする
+    special_escaped_localization = _(tmp_directory_path, "special_escaped_localization")
+    special_escape(source_dir_path=replaced_localization_dir_path,
+                   output_dir_path=special_escaped_localization)
+
+    print("Finish special encoding")
+
     # AppModを構築する
     app_mod_zip_file_path = assembly_app_mod_zip_file(
-        resource_paratranz_trans_zip_file_path=p_file_path,
+        source_dir_path=special_escaped_localization,
         resource_image_file_path=_(".", "resource", "title.jpg"),
-        out_file_path=_(".", "tmp", "mod"))
+        out_directory_path=tmp_directory_path)
 
     print("app_mod_zip_file_path:{}".format(app_mod_zip_file_path))
 
@@ -244,6 +327,7 @@ def main():
 
     # GoogleDriveにアップロード from datetime import datetime as dt
     from datetime import datetime as dt
+
     cdn_url = upload_mod_to_google_drive(
         upload_file_path=mod_pack_file_path,
         name=dt.now().strftime('%Y-%m-%d_%H-%M-%S-{}.zip'.format("eu4-ap1")),
@@ -256,8 +340,8 @@ def main():
                                out_file_path=_(".", "out", "dist.v2.json"),
                                mod_file_path=mod_pack_file_path)
 
-    # utf8ファイルを移動する（この後git pushする）
-    update_source(resource_paratranz_trans_zip_file_path=p_file_path)
+    # git pushするために移動する
+    shutil.copytree("./", )
 
 
 if __name__ == "__main__":
---
+++ special_escape.py
@@ -0,0 +1,424 @@
+import argparse
+import os
+import pathlib
+
+eu4_escape_targets = [
+    0xA4,
+    0xA3,
+    0xA7,
+    0x24,
+    0x5B,
+    0x00,
+    0x5C,
+    0x20,
+    0x0D,
+    0x0A,
+    0x22,
+    0x7B,
+    0x7D,
+    0x40,
+    0x3B,
+    0x80,
+    0x7E,
+    0xBD,
+    0x5F
+]
+
+ck2_escape_targets = [
+    0xA4,
+    0xA3,
+    0xA7,
+    0x24,
+    0x5B,
+    0x00,
+    0x5C,
+    0x20,
+    0x0D,
+    0x0A,
+    0x22,
+    0x7B,
+    0x7D,
+    0x40,
+    0x3B,
+    0x80,
+    0x7E,
+    0xBD,
+    0x5F
+]
+
+
+def generate_encoder(game_type, ext):
+    """
+
+    :param game_type: ゲーム種別
+    :param ext: 拡張子
+    :return:
+    """
+
+    # 定義
+    if game_type == "eu4":
+        escape_targets = eu4_escape_targets
+        high_byte_shift = -9
+        low_byte_shift = 14
+    elif game_type == "ck2":
+        escape_targets = ck2_escape_targets
+        high_byte_shift = -9
+        low_byte_shift = 15
+    else:
+        raise Exception("typeが不明")
+
+    def ___(src_array):
+        return __(map(ucs_to_cp1252, src_array))
+
+    def ____(src_array):
+        return map(cp1252_to_ucs2, __(src_array))
+
+    def __(src_array):
+        """
+        変換器
+        :param src_array: コードポイント配列
+        :return:
+        """
+        result = []
+        for code_point in src_array:
+
+            # BMP外の文字
+            if code_point > 0xFFFF:
+                print("not convert character")
+                continue
+
+            # null文字
+            if code_point == 0:
+                print("Found null character")
+                continue
+
+            high_byte = (code_point >> 8) & 0x000000FF
+            low_byte = code_point & 0x000000FF
+
+            # 2byteじゃない
+            if high_byte == 0:
+                result.append(code_point)
+                continue
+
+            escape_char = 0x10
+
+            if high_byte in escape_targets:
+                escape_char += 2
+
+            if low_byte in escape_targets:
+                escape_char += 1
+
+            if escape_char == 0x11:
+                low_byte = low_byte + low_byte_shift
+            elif escape_char == 0x12:
+                high_byte = high_byte + high_byte_shift
+            elif escape_char == 0x13:
+                low_byte = low_byte + low_byte_shift
+                high_byte = high_byte + high_byte_shift
+            else:
+                pass
+
+            result.append(escape_char)
+            result.append(low_byte)
+            result.append(high_byte)
+
+        return result
+
+    if game_type == "eu4":
+        if ext == "yml":
+            return ____
+        elif ext == "txt":
+            return ___
+        else:
+            raise Exception()
+
+    elif game_type == "ck2":
+        if ext in ["csv", "txt"]:
+            return ___
+        else:
+            raise Exception()
+    else:
+        raise Exception()
+
+
+ucs2_to_cp1252_table = {
+    0x20AC: 0x80,  # €
+    # 0x81
+    0x201A: 0x82,  # ‚
+    0x0192: 0x83,  # ƒ
+    0x201E: 0x84,  # „
+    0x2026: 0x85,  # …
+    0x2020: 0x86,  # †
+    0x2021: 0x87,  # ‡
+    0x02C6: 0x88,  # ˆ
+    0x2030: 0x89,  # ‰
+    0x0160: 0x8A,  # Š
+    0x2039: 0x8B,  # ‹
+    0x0152: 0x8C,  # Œ
+    # 0x8D
+    0x017D: 0x8E,  # Ž
+    # 0x8F
+    # 0x90
+    0x2018: 0x91,  # ‘
+    0x2019: 0x92,  # ’
+    0x201C: 0x93,  # “
+    0x201D: 0x94,  # ”
+    0x2022: 0x95,  # •
+    0x2013: 0x96,  # –
+    0x2014: 0x97,  # —
+    0x02DC: 0x98,  # ˜
+    0x2122: 0x99,  # ™
+    0x0161: 0x9A,  # š
+    0x203A: 0x9B,  # ›
+    0x0153: 0x9C,  # œ
+    # 0x9D
+    0x017E: 0x9E,  # ž
+    0x0178: 0x9F  # Ÿ
+}
+
+cp1252_to_ucs2_table = {
+    0x80: 0x20AC,  # €
+    # 0x81
+    0x82: 0x201A,  # ‚
+    0x83: 0x0192,  # ƒ
+    0x84: 0x201E,  # „
+    0x85: 0x2026,  # …
+    0x86: 0x2020,  # †
+    0x87: 0x2021,  # ‡
+    0x88: 0x02C6,  # ˆ
+    0x89: 0x2030,  # ‰
+    0x8A: 0x0160,  # Š
+    0x8B: 0x2039,  # ‹
+    0x8C: 0x0152,  # Œ
+    # 0x8D
+    0x8E: 0x017D,  # Ž
+    # 0x8F
+    # 0x90
+    0x91: 0x2018,  # ‘
+    0x92: 0x2019,  # ’
+    0x93: 0x201C,  # “
+    0x94: 0x201D,  # ”
+    0x95: 0x2022,  # •
+    0x96: 0x2013,  # –
+    0x97: 0x2014,  # —
+    0x98: 0x02DC,  # ˜
+    0x99: 0x2122,  # ™
+    0x9A: 0x0161,  # š
+    0x9B: 0x203A,  # ›
+    0x9C: 0x0153,  # œ
+    # 0x9D
+    0x9E: 0x017E,  # ž
+    0x9F: 0x0178  # Ÿ
+}
+
+
+def ucs_to_cp1252(code_point):
+    if code_point in ucs2_to_cp1252_table:
+        return ucs2_to_cp1252_table[code_point]
+    else:
+        return code_point
+
+
+def cp1252_to_ucs2(code_point):
+    if code_point in cp1252_to_ucs2_table:
+        return cp1252_to_ucs2_table[code_point]
+    else:
+        return code_point
+
+
+def generate_printer(game_type, ext):
+    """
+    プリンター生成器
+    :param game_type: ゲーム種別
+    :param ext: 拡張子
+    :return: プリンター
+    """
+
+    def utf8_printer(src_array, out_file_path):
+        with open(out_file_path, "wt", encoding="utf_8_sig") as fw:
+            text = "".join(map(chr, src_array))
+            fw.write(text)
+
+    def cp1252_like_printer(src_array, out_file_path):
+        """
+        純粋にCP1252ではない。（規定されていない0x81や0x90などのコードポイントも出現しうる）、
+        バイナリモードで書き込む
+        :param src_array:
+        :param out_file_path:
+        :return:
+        """
+
+        w = bytearray(map(ucs_to_cp1252, src_array))
+
+        with open(out_file_path, "wb") as fw:
+            fw.write(w)
+
+    if game_type == "eu4":
+        if ext == "yml":
+            return utf8_printer
+        elif ext == "txt":
+            return cp1252_like_printer
+        else:
+            raise Exception()
+
+    elif game_type == "ck2":
+        if ext in ["csv", "txt"]:
+            return cp1252_like_printer
+        else:
+            raise Exception()
+    else:
+        raise Exception()
+
+
+def target_is_directory(params):
+    is_out_dir = os.path.isdir(str(params.out))
+
+    # 走査
+    for file_path in pathlib.Path(params.src).glob('**/*.*'):
+        if file_path.suffix not in ['.yml', '.csv', '.txt']:
+            continue
+
+        # 指定がない場合は、ソースと同じ場所に、同じ名前で拡張子を.encodedにしたものを保存する
+        if params.out is None:
+            out_file_path = os.path.join(
+                os.path.dirname(os.path.abspath(str(file_path))),
+                os.path.basename(str(file_path)) + ".encode"
+            )
+
+        # 出力先が存在するディレクトリ
+        elif is_out_dir:
+            out_file_path = os.path.join(
+                str(params.out),
+                str(file_path).replace(str(params.src) + "\\", "")
+            )
+
+            dir_path = os.path.dirname(str(out_file_path))
+            if not os.path.exists(dir_path):
+                os.makedirs(dir_path)
+
+        else:
+            raise Exception("出力先が不正")
+
+        do_file(in_file_path=file_path,
+                out_file_path=out_file_path,
+                encoder=generate_encoder(params.type, os.path.splitext(str(file_path))[1][1:]),
+                printer=generate_printer(params.type, os.path.splitext(str(file_path))[1][1:]),
+                is_bom=params.bom)
+
+
+def target_is_file(params):
+    # 指定がない場合は、ソースと同じ場所に、同じ名前で拡張子を.utf8にしたものを保存する
+    if params.out is None:
+        out_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(params.src)),
+            os.path.basename(params.src) + ".encode"
+        )
+
+    # 指定先が存在するディレクトリの場合は、そこに同じ名前で保存する
+    elif os.path.isdir(params.out):
+        out_file_path = os.path.join(
+            params.out,
+            os.path.basename(params.src)
+        )
+
+    # 指定先がフルパス
+    elif params.out != "":
+        out_file_path = params.out
+    else:
+        raise Exception("出力先が不正")
+
+    do_file(in_file_path=params.src,
+            out_file_path=out_file_path,
+            encoder=generate_encoder(params.type, os.path.splitext(str(params.src))[1][1:]),
+            printer=generate_printer(params.type, os.path.splitext(str(params.src))[1][1:]),
+            is_bom=params.bom)
+
+
+def do_file(in_file_path, out_file_path, encoder, printer, is_bom):
+    """
+    ファイルを変換
+    :param in_file_path: 入力先ファイルパス
+    :param out_file_path: 出力先ファイルパス
+    :param encoder: エンコーダー
+    :param printer: プリンター
+    :param is_bom : bom付きかどうか
+    :return:
+    """
+
+    if is_bom:
+        encoding = 'utf_8_sig'
+    else:
+        encoding = 'utf_8'
+
+    # 読み込み
+    with open(in_file_path, "rt", encoding=encoding) as fr:
+        printer(src_array=encoder(src_array=map(ord, fr.read())),
+                out_file_path=out_file_path)
+
+
+def generate_default_arg_parser():
+    """
+    argumentsパーサーを作成
+    :return: パーサー
+    """
+
+    # title
+    result = argparse.ArgumentParser(
+        description='Process some integers.'
+    )
+
+    # ソース
+    result.add_argument(
+        'src',
+        metavar='src',
+        type=str,
+        help='source file or directory.')
+
+    # 出力先
+    result.add_argument(
+        '-out',
+        metavar='X',
+        dest='out',
+        help='output directory')
+
+    # タイプ
+    result.add_argument(
+        '-type',
+        metavar='X',
+        dest='type',
+        default="eu4",
+        help='eu4 or ck2')
+
+    # bomがある
+    result.add_argument(
+        '--bom',
+        action='store_true',
+        dest='bom',
+        help='utf8 with bom')
+
+    return result
+
+
+def special_escape(arg_params):
+    """
+    main
+    :return:
+    """
+
+    # fileかdirか
+    if os.path.isfile(arg_params.src):
+        target_is_file(params=arg_params)
+    elif os.path.isdir(arg_params.src):
+        target_is_directory(params=arg_params)
+    else:
+        raise Exception("srcがみつからない")
+
+
+if __name__ == '__main__':
+    """
+    entry-point
+    """
+    parser = generate_default_arg_parser()
+    params = parser.parse_args()
+
+    special_escape(arg_params=params)
