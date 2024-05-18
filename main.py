15c9d485a406086b3abed1e1f81fc39f818a8ecd
matanki-saito <35730970+matanki-saito@users.noreply.github.com>
2024-05-19 01:12:07 +0900

    Update main.py
    1.36 -> 1.37

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ def main():
         mod_title_name="JPMOD Main 2: Text",
         mod_tags={"Translation", "Localisation"},
         mod_image_file_path="title.jpg",
-        mod_supported_version="1.36.*.*")
+        mod_supported_version="1.37.*.*")
 
     print("mod_pack_file_path:{}".format(mod_pack_file_path))
 
