326bb276acf28817701caf799fa07d1e820d721a
matanki-saito <35730970+matanki-saito@users.noreply.github.com>
2024-09-20 20:34:36 +0900

    KoK変更を反映

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ replace_parameter_map_definition = {
 
     "GetJanissariesName": "GetイェニチェリName",
     "GetEunuchsName": "Get宦官Name",
+    "GetQizilbashName": "GetクズルバシュName",
+    "GetGhilmanName": "GetグラームName"
     
     " ": " "
 }
